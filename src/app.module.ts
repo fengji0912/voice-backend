86a85f0e69cb1855151089251dce1a5f4044f52b
fengji0912 <141588372+fengji0912@users.noreply.github.com>
2024-09-14 20:03:58 +0000

    module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { RecommendationsModule } from './recommendations/recommendations.module';
+import { DataModule } from './data/data.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, RecommendationsModule, DataModule],
   controllers: [AppController],
   providers: [AppService],
 })
